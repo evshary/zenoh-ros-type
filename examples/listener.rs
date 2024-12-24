1915c44b1516c059cfd2fc135bb75e2d6407368b
ChenYing Kuo <evshary@gmail.com>
2024-12-24 10:22:13 +0800

    Add listener.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

---
+++ examples/listener.rs
@@ -0,0 +1,17 @@
+use zenoh::{Config, Wait};
+use zenoh_ros_type::common_interfaces::std_msgs;
+
+fn main() {
+    let key_expr = "chatter";
+    let session = zenoh::open(Config::default()).wait().unwrap();
+    let _subscriber = session
+        .declare_subscriber(key_expr)
+        .callback(|sample| {
+            let msg: std_msgs::String_ =
+                cdr::deserialize_from(sample.payload().reader(), cdr::size::Infinite).unwrap();
+            println!("Receive: {}", msg.data);
+        })
+        .wait()
+        .unwrap();
+    std::thread::park();
+}
